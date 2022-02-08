f0846b95ec5dc0de713199e57fbfdc240a98de1c
azh <2550364626@qq.com>
2022-02-08 14:55:21 +0800

    3.Spring Boot yaml配置

--- studyrecord/springboot/first_spring_boot_prj/src/main/java/com/azh/FirstSpringBootPrjApplication.java
+++ studyrecord/springboot/first_spring_boot_prj/src/main/java/com/azh/FirstSpringBootPrjApplication.java
@@ -13,7 +13,7 @@ public class FirstSpringBootPrjApplication {
     public static void main(String[] args) {
         // ----设置 Banner 是否启动 start----
         SpringApplicationBuilder banner = new SpringApplicationBuilder(FirstSpringBootPrjApplication.class);
-        banner.bannerMode(Banner.Mode.OFF).run(args);
+        banner.bannerMode(Banner.Mode.LOG).run(args);
         // ---- end----
 
         // ----默认启动 start----
