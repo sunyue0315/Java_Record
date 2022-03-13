8641c65749949f22b7bdedb682dd502393d43b7b
azh <2550364626@qq.com>
2022-03-13 23:41:43 +0800

    1.非线程安全

--- studyrecord/juc/createThread/src/main/java/com/anzhi/threadcreate/CreateThreadByExtendsThread.java
+++ studyrecord/juc/createThread/src/main/java/com/anzhi/threadcreate/CreateThreadByExtendsThread.java
@@ -3,7 +3,6 @@ package com.anzhi.threadcreate;
 public class CreateThreadByExtendsThread extends Thread{
     @Override
     public void run() {
-        super.run();
         System.out.println("Create Thread by Extends Thread Begin to Run!");
     }
 }
