d625d7a6d2ac8627dfc03aea143051a1c8c64786
azh <2550364626@qq.com>
2022-03-19 16:47:16 +0800

    1.Thread 方法

--- studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/CurrentThreadMethod.java
+++ studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/CurrentThreadMethod.java
@@ -2,4 +2,14 @@ package com.anzhi.threadmethod;
 
 public class CurrentThreadMethod extends Thread{
 
+    public CurrentThreadMethod(){
+        System.out.println("构造方法的打印，当前线程执行名称：: " + Thread.currentThread().getName());
+        System.out.println("构造方法的打印，当前类的名称：" + this.getName());
+    }
+
+    @Override
+    public void run() {
+        System.out.println("run 方法打印, 当前线程执行名称：" + Thread.currentThread().getName());
+        System.out.println("run 方法打印，当前类的名称" + this.getName());
+    }
 }
