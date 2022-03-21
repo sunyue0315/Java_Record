90c775aca519a6eb7fffeb8072de9dae0970dd7a
azh <2550364626@qq.com>
2022-03-21 11:17:33 +0800

    1.线程方法

--- studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/getallstacktraces/GetAllStackTracesMethod.java
+++ studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/getallstacktraces/GetAllStackTracesMethod.java
@@ -1,5 +1,54 @@
 package com.anzhi.threadmethod.getallstacktraces;
 
-public class GetAllStackTracesMethod extends Thread{
+import java.util.Iterator;
+import java.util.Map;
 
+public class GetAllStackTracesMethod extends Thread{
+    private void a(){
+        b();
+    }
+
+    private void b() {
+        c();
+    }
+
+    private void c() {
+        d();
+    }
+
+    private void d() {
+        e();
+    }
+
+    private void e() {
+        Map<Thread,StackTraceElement[]> map = Thread.currentThread().getAllStackTraces();
+        if(map != null && map.size() != 0){
+            Iterator keyIterator = map.keySet().iterator();
+            while(keyIterator.hasNext()){
+                Thread eachThread = (Thread) keyIterator.next();
+                StackTraceElement[] array = map.get(eachThread);
+                System.out.println("-----------------每个线程的基本信息");
+                System.out.println("线程名称：" + eachThread.getName());
+                System.out.println("线程分组：" + eachThread.getThreadGroup().getName());
+                System.out.println("StackTraceElement[].length= " + array.length);
+                System.out.println("线程的状态：" + eachThread.getState());
+
+                if(array.length != 0){
+                    System.out.println("输出 StackTraceElement[] 数组具体信息：");
+                    for (StackTraceElement element : array) {
+                        System.out.println("className= " + element.getClassName() + "  fileName=" +
+                                element.getFileName() + "   lineNumber= " + element.getLineNumber() + "   methodName=: " + element.getMethodName());
+                    }
+                }else{
+                    System.out.println("没有StackTraceElement[] 信息，因为线程：" + eachThread.getName()
+                            + "中的StackTraceElement[].length=0");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        a();
+    }
 }
---
+++ studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/getallstacktraces/GetAllStackTracesMethodTest.java
@@ -0,0 +1,9 @@
+package com.anzhi.threadmethod.getallstacktraces;
+
+public class GetAllStackTracesMethodTest {
+    public static void main(String[] args) {
+        GetAllStackTracesMethod stackTracesMethod = new GetAllStackTracesMethod();
+        stackTracesMethod.setName("GetAllStackTracesMethodTest");
+        stackTracesMethod.start();
+    }
+}
