7c1f9d267187e5608dc28bec2ca58d0e550f0f1f
azh <2550364626@qq.com>
2022-03-21 19:26:47 +0800

    1.stop方法

--- studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/stopthread/stopreleaselock/StopReleaseLockThreadTest.java
+++ studyrecord/juc/createThread/src/main/java/com/anzhi/threadmethod/stopthread/stopreleaselock/StopReleaseLockThreadTest.java
@@ -2,6 +2,18 @@ package com.anzhi.threadmethod.stopthread.stopreleaselock;
 
 public class StopReleaseLockThreadTest {
     public static void main(String[] args) {
-
+        StopReleaseLockService stopReleaseLockService = new StopReleaseLockService();
+        StopReleaseLockThreadA threadA = new StopReleaseLockThreadA(stopReleaseLockService);
+        threadA.start();
+        try {
+            Thread.sleep(100);
+            StopReleaseLockThreadB stopReleaseLockThreadB = new StopReleaseLockThreadB(stopReleaseLockService);
+            StopReleaseLockThreadB threadB = new StopReleaseLockThreadB(stopReleaseLockService);
+            threadB.start();
+            Thread.sleep(3000);
+            threadA.stop();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
