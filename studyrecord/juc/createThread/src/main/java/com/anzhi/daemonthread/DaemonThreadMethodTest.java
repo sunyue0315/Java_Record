ef41a4793b00881d3070b3f8005b95d45a88c7b2
azh <2550364626@qq.com>
2022-03-23 13:37:02 +0800

    1.守护线程

--- studyrecord/juc/createThread/src/main/java/com/anzhi/daemonthread/DaemonThreadMethodTest.java
+++ studyrecord/juc/createThread/src/main/java/com/anzhi/daemonthread/DaemonThreadMethodTest.java
@@ -3,6 +3,8 @@ package com.anzhi.daemonthread;
 public class DaemonThreadMethodTest extends Thread{
     public static void main(String[] args) {
         try{
+            // 设置一个钩子线程， 在 JVM 退出时输出日志
+            Runtime.getRuntime().addShutdownHook(new Thread(()-> System.out.println("The JVM exit success !!!")));
             DaemonThreadMethod threadMethod = new DaemonThreadMethod();
             threadMethod.setDaemon(true);
             threadMethod.start();
